3ce347c35fd45aab9da0c3f94c5c4b4e0bc49d44
Michael Crosby <michael@crosbymichael.com>
2014-05-14 15:21:44 -0700

    Move cgroups package into libcontainer Docker-DCO-1.1-Signed-off-by: Michael Crosby <michael@crosbymichael.com> (github: crosbymichael)

---
+++ cgroups/systemd/apply_nosystemd.go
@@ -0,0 +1,17 @@
+// +build !linux
+
+package systemd
+
+import (
+	"fmt"
+
+	"github.com/dotcloud/docker/pkg/libcontainer/cgroups"
+)
+
+func UseSystemd() bool {
+	return false
+}
+
+func Apply(c *Cgroup, pid int) (cgroups.ActiveCgroup, error) {
+	return nil, fmt.Errorf("Systemd not supported")
+}
