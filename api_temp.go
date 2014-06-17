173642ab0b662286fa2ae4c47b2c78a908716363
Victor Marmol <vmarmol@google.com>
2014-06-16 21:53:39 -0700

    Basic version of libcontainer API.
    This version has mostly the behavior available today and the
    functionality agreed upon in other PRs. Further changes will be sent as
    smaller PRs for discussion.
    
    Docker-DCO-1.1-Signed-off-by: Victor Marmol <vmarmol@google.com> (github: vmarmol)

---
+++ api_temp.go
@@ -0,0 +1,29 @@
+/*
+Temporary API endpoint for libcontainer while the full API is finalized (api.go).
+*/
+package libcontainer
+
+import (
+	"github.com/docker/libcontainer/cgroups/fs"
+	"github.com/docker/libcontainer/network"
+)
+
+// TODO(vmarmol): Complete Stats() in final libcontainer API and move users to that.
+// DEPRECATED: The below portions are only to be used during the transition to the official API.
+
+// Returns all available stats for the given container.
+func GetStats(container *Config, state *State) (*ContainerStats, error) {
+	var containerStats ContainerStats
+	stats, err := fs.GetStats(container.Cgroups)
+	if err != nil {
+		return &containerStats, err
+	}
+	containerStats.CgroupStats = stats
+	networkStats, err := network.GetStats(&state.NetworkState)
+	if err != nil {
+		return &containerStats, err
+	}
+	containerStats.NetworkStats = networkStats
+
+	return &containerStats, nil
+}
