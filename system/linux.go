a2dc64cf30831fc78813215e7317a086341856c5
Michael Crosby <michael@docker.com>
2014-07-14 16:46:51 -0700

    Add initial system pkg to libcontainer
    Port over console and ptmx code into console package
    Docker-DCO-1.1-Signed-off-by: Michael Crosby <michael@docker.com> (github: crosbymichael)

---
+++ system/linux.go
@@ -0,0 +1,17 @@
+// +build linux
+
+package system
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func Execv(cmd string, args []string, env []string) error {
+	name, err := exec.LookPath(cmd)
+	if err != nil {
+		return err
+	}
+
+	return syscall.Exec(name, args, env)
+}
