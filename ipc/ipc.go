ad9b433e6c5250010b4515a8c8cf3972ffef63d7
Michael Crosby <crosbymichael@gmail.com>
2014-10-28 22:08:04 +0000

    Add test for ipc private and shared path
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- ipc/ipc.go
+++ ipc/ipc.go
@@ -11,7 +11,6 @@ import (
 // Join the IPC Namespace of specified ipc path if it exists.
 // If the path does not exist then you are not joining a container.
 func Initialize(nsPath string) error {
-
 	if nsPath == "" {
 		return nil
 	}
@@ -26,6 +25,5 @@ func Initialize(nsPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to setns current IPC namespace: %v", err)
 	}
-
 	return nil
 }
