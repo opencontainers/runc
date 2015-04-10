38ef9ee346eae3b9bb1d667390fcacc2a89d2db3
Adam Conrad <adconrad@0c3.net>
2015-04-10 12:48:48 -0600

    Add support for ARMv8 (AKA: arm64 or Aarch64)
    Signed-off-by: Adam Conrad <adconrad@0c3.net>

--- system/setns_linux.go
+++ system/setns_linux.go
@@ -12,6 +12,7 @@ import (
 // We are declaring the macro here because the SETNS syscall does not exist in th stdlib
 var setNsMap = map[string]uintptr{
 	"linux/386":     346,
+	"linux/arm64":   268,
 	"linux/amd64":   308,
 	"linux/arm":     374,
 	"linux/ppc64":   350,
