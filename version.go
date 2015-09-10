580701c6f8aa20071e93e2bc084787f39dcb00ae
Vincent Batts <vbatts@hashbangbash.com>
2015-09-10 11:17:34 -0400

    version: more explicit version for comparison
    For deterministic comparison and still providing a Version string for
    representation and reuse.
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- version.go
+++ version.go
@@ -1,4 +1,15 @@
 package specs
 
+import "fmt"
+
+const (
+	// VersionMajor is for an API incompatible changes
+	VersionMajor = 0
+	// VersionMinor is for functionality in a backwards-compatible manner
+	VersionMinor = 1
+	// VersionPatch is for backwards-compatible bug fixes
+	VersionPatch = 0
+)
+
 // Version is the specification version that the package types support.
-const Version = "pre-draft"
+var Version = fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
