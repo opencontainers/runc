77c36f4b348671e2f61132dbb9147479fd421ef4
David Calavera <david.calavera@gmail.com>
2015-12-15 15:12:29 -0500

    Move linux only Process.InitializeIO behind the linux build flag.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- libcontainer/process.go
+++ libcontainer/process.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"math"
 	"os"
-	"syscall"
 )
 
 type processOperations interface {
@@ -84,47 +83,6 @@ type IO struct {
 	Stdin  io.WriteCloser
 	Stdout io.ReadCloser
 	Stderr io.ReadCloser
-}
-
-// InitializeIO creates pipes for use with the process's STDIO
-// and returns the opposite side for each
-func (p *Process) InitializeIO(rootuid int) (i *IO, err error) {
-	var fds []uintptr
-	i = &IO{}
-	// cleanup in case of an error
-	defer func() {
-		if err != nil {
-			for _, fd := range fds {
-				syscall.Close(int(fd))
-			}
-		}
-	}()
-	// STDIN
-	r, w, err := os.Pipe()
-	if err != nil {
-		return nil, err
-	}
-	fds = append(fds, r.Fd(), w.Fd())
-	p.Stdin, i.Stdin = r, w
-	// STDOUT
-	if r, w, err = os.Pipe(); err != nil {
-		return nil, err
-	}
-	fds = append(fds, r.Fd(), w.Fd())
-	p.Stdout, i.Stdout = w, r
-	// STDERR
-	if r, w, err = os.Pipe(); err != nil {
-		return nil, err
-	}
-	fds = append(fds, r.Fd(), w.Fd())
-	p.Stderr, i.Stderr = w, r
-	// change ownership of the pipes incase we are in a user namespace
-	for _, fd := range fds {
-		if err := syscall.Fchown(int(fd), rootuid, rootuid); err != nil {
-			return nil, err
-		}
-	}
-	return i, nil
 }
 
 // NewConsole creates new console for process and returns it
