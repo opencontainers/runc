467fd17d4f2a987fa00051ce44b69bf9620e2ee6
Vincent Batts <vbatts@hashbangbash.com>
2016-01-05 16:52:01 -0500

    version: introduce a string for dev indication
    Include a changelog of commit subjects
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- version.go
+++ version.go
@@ -9,7 +9,10 @@ const (
 	VersionMinor = 2
 	// VersionPatch is for backwards-compatible bug fixes
 	VersionPatch = 0
+
+	// VersionDev indicates development branch. Releases will be empty string.
+	VersionDev = ""
 )
 
 // Version is the specification version that the package types support.
-var Version = fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+var Version = fmt.Sprintf("%d.%d.%d%s", VersionMajor, VersionMinor, VersionPatch, VersionDev)
