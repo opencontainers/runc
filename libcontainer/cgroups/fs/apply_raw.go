47f16e89df2aedc71dba3090dafb3d11f7d4dcbc
Michael Crosby <crosbymichael@gmail.com>
2016-02-19 11:23:30 -0800

    Move the process outside of the systemd cgroup
    If you don't move the process out of the named cgroup for systemd then
    systemd will try to delete all the cgroups that the process is currently
    in.
    
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- libcontainer/cgroups/fs/apply_raw.go
+++ libcontainer/cgroups/fs/apply_raw.go
@@ -31,6 +31,7 @@ var (
 		&NetPrioGroup{},
 		&PerfEventGroup{},
 		&FreezerGroup{},
+		&NameGroup{GroupName: "name=systemd", Join: true},
 	}
 	CgroupProcesses  = "cgroup.procs"
 	HugePageSizes, _ = cgroups.GetHugePageSize()
