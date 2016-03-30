9da17282d97a4b38dc5709231112c113cb539018
Vincent Batts <vbatts@hashbangbash.com>
2016-03-30 14:49:29 -0400

    schema: add a README
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- schema/validate.go
+++ schema/validate.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	if len(os.Args[1:]) != 2 {
-		fmt.Printf("ERROR: usage is: %s <schema.json> <config.json>", os.Args[0])
+		fmt.Printf("ERROR: usage is: %s <schema.json> <config.json>\n", os.Args[0])
 		os.Exit(1)
 	}
 
