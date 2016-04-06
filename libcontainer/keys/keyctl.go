c27bc734b23e22833e0c6e203cb26f604842939e
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2016-04-06 16:48:46 -0700

    Increase keyring quota automatically once it is reached
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- libcontainer/keys/keyctl.go
+++ libcontainer/keys/keyctl.go
@@ -4,9 +4,10 @@ package keyctl
 
 import (
 	"fmt"
-	"syscall"
-	"strings"
+	"io/ioutil"
 	"strconv"
+	"strings"
+	"syscall"
 	"unsafe"
 )
 
@@ -15,6 +16,88 @@ const KEYCTL_SETPERM = 5
 const KEYCTL_DESCRIBE = 6
 
 type KeySerial uint32
+
+func updateKeyringQuota(keyNameLen int) error {
+	b, err := ioutil.ReadFile("/proc/key-users")
+	if err != nil {
+		return fmt.Errorf("failed to open /proc/key-users: %v", err)
+	}
+	str := string(b)
+	str = strings.Trim(str, " \t")
+	lines := strings.Split(str, "\n")
+	for _, line := range lines {
+		ar := strings.Split(line, " ")
+		// idx 1 is uid
+		// idx len - 1 is maxbyte quota
+		// idx len - 2 is maxkeys count
+		// we run as root, so only care about uid 0
+		if fmt.Sprintf("%d:", 0) == ar[0] {
+			var (
+				path   string
+				status []string
+				cb     int64 // How many bytes are currently used by keys
+				mb     int64 // Maximun allowed bytes for user
+				ck     int64 // How many keys are in used for the user
+				mk     int64 // Maximum allowed number of keys for user
+				err    error
+			)
+			// Check maxbytes first
+			status = strings.Split(ar[len(ar)-1], "/")
+			if len(status) != 2 {
+				return fmt.Errorf("unexpected format /proc/key-users format: %s", line)
+			}
+			cb, err = strconv.ParseInt(status[0], 10, 32)
+			if err != nil {
+				return fmt.Errorf("failed to convert current bytes value: %v", err)
+			}
+			mb, err = strconv.ParseInt(status[1], 10, 32)
+			if err != nil {
+				return fmt.Errorf("failed to convert max bytes value: %v", err)
+			}
+
+			// The kernel add 1 byte internally
+			if cb == mb || cb+int64(keyNameLen)+1 >= mb {
+				// Add 10% more quota
+				mb = mb + (mb * 10 / 100)
+				path = "/proc/sys/kernel/keys/root_maxbytes"
+				err = ioutil.WriteFile(path, []byte(fmt.Sprintf("%d", uint32(mb))), 0644)
+				if err != nil {
+					return fmt.Errorf("failed to update %s: %v", path, err)
+				}
+			}
+
+			// Now check maxkeys
+			status = strings.Split(ar[len(ar)-2], "/")
+			if len(status) != 2 {
+				return fmt.Errorf("unexpected format /proc/key-users format: %s", line)
+			}
+			ck, err = strconv.ParseInt(status[0], 10, 32)
+			if err != nil {
+				return fmt.Errorf("failed to convert current key count value: %v", err)
+			}
+			mk, err = strconv.ParseInt(status[1], 10, 32)
+			if err != nil {
+				return fmt.Errorf("failed to convert max key count value: %v", err)
+			}
+
+			// On some machines the error was seen when 199/200 was
+			// reached so add 1 for good measure
+			if ck+1 >= mk {
+				// Add 10% more quota
+				mk = mk + (mk * 10 / 100)
+				path = "/proc/sys/kernel/keys/root_maxkeys"
+				err = ioutil.WriteFile(path, []byte(fmt.Sprintf("%d", uint32(mk))), 0644)
+				if err != nil {
+					return fmt.Errorf("failed to update %s: %v", path, err)
+				}
+			}
+
+			break
+		}
+	}
+
+	return nil
+}
 
 func JoinSessionKeyring(name string) (KeySerial, error) {
 	var _name *byte = nil
@@ -27,7 +110,14 @@ func JoinSessionKeyring(name string) (KeySerial, error) {
 		}
 	}
 
+loop:
 	sessKeyId, _, errn := syscall.Syscall(syscall.SYS_KEYCTL, KEYCTL_JOIN_SESSION_KEYRING, uintptr(unsafe.Pointer(_name)), 0)
+	if errn == syscall.EDQUOT {
+		if err := updateKeyringQuota(len(name)); err != nil {
+			return KeySerial(0), fmt.Errorf("failed to raised keyring quota: %v", err)
+		}
+		goto loop
+	}
 	if errn != 0 {
 		return 0, fmt.Errorf("could not create session key: %v", errn)
 	}
@@ -64,4 +154,3 @@ func ModKeyringPerm(ringId KeySerial, mask, setbits uint32) error {
 
 	return nil
 }
-
