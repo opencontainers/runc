50dbe5f47443ad12483c27a05aafa86dd539ff3e
Mike Brown <brownwm@us.ibm.com>
2016-07-11 09:31:07 -0500

    address issue #797 by adding additional documentation
    Signed-off-by: Mike Brown <brownwm@us.ibm.com>

--- list.go
+++ list.go
@@ -38,6 +38,18 @@ type containerState struct {
 var listCommand = cli.Command{
 	Name:  "list",
 	Usage: "lists containers started by runc with the given root",
+	ArgsUsage: `
+
+Where the given root is specified via the global option "--root"
+(default: "/run/runc").
+
+EXAMPLE 1:
+To list containers created via the default "--root":
+       runc list
+
+EXAMPLE 2:
+To list containers created using a non-default value for "--root":
+       runc --root value list`,
 	Flags: []cli.Flag{
 		cli.StringFlag{
 			Name:  "format, f",
