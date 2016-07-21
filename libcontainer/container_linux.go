2cd9c31b995cff313c828dafbfeea082c7318c3f
Aleksa Sarai <asarai@suse.de>
2016-07-21 10:02:08 +1000

    nsenter: guarantee correct user namespace ordering
    Depending on your SELinux setup, the order in which you join namespaces
    can be important. In general, user namespaces should *always* be joined
    and unshared first because then the other namespaces are correctly
    pinned and you have the right priviliges within them. This also is very
    useful for rootless containers, as well as older kernels that had
    essentially broken unshare(2) and clone(2) implementations.
    
    This also includes huge refactorings in how we spawn processes for
    complicated reasons that I don't want to get into because it will make
    me spiral into a cloud of rage. The reasoning is in the giant comment in
    clone_parent. Have fun.
    
    In addition, because we now create multiple children with CLONE_PARENT,
    we cannot wait for them to SIGCHLD us in the case of a death. Thus, we
    have to resort to having a child kindly send us their exit code before
    they die. Hopefully this all works okay, but at this point there's not
    much more than we can do.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- libcontainer/container_linux.go
+++ libcontainer/container_linux.go
@@ -1224,12 +1224,13 @@ func (c *linuxContainer) currentState() (*State, error) {
 func (c *linuxContainer) orderNamespacePaths(namespaces map[configs.NamespaceType]string) ([]string, error) {
 	paths := []string{}
 	order := []configs.NamespaceType{
+		// The user namespace *must* be done first.
+		configs.NEWUSER,
 		configs.NEWIPC,
 		configs.NEWUTS,
 		configs.NEWNET,
 		configs.NEWPID,
 		configs.NEWNS,
-		configs.NEWUSER,
 	}
 
 	// Remove namespaces that we don't need to join.
