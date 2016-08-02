777ac05e5e1bcdf01e3750e489b27df60c1be8e5
Qiang Huang <h.huangqiang@huawei.com>
2016-08-02 09:04:30 +0800

    Cleanup GetLongBit
    Follow up: #962
    
    Signed-off-by: Qiang Huang <h.huangqiang@huawei.com>

--- libcontainer/system/sysconfig.go
+++ libcontainer/system/sysconfig.go
@@ -4,21 +4,6 @@ package system
 
 /*
 #include <unistd.h>
-#include <limits.h>
-
-int GetLongBit() {
-#ifdef _SC_LONG_BIT
-    int longbits;
-
-    longbits = sysconf(_SC_LONG_BIT);
-    if (longbits <  0) {
-        longbits = (CHAR_BIT * sizeof(long));
-    }
-    return longbits;
-#else
-    return (CHAR_BIT * sizeof(long));
-#endif
-}
 */
 import "C"
 
