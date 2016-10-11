a27fa6ae18bdbfe35fa7569bfd162a32f78a9cda
Aleksa Sarai <asarai@suse.de>
2016-10-11 22:10:53 +1100

    libcontainer: system: add GetProcessState and GetProcessParent
    Similar to the already existing proc functions, these just get different
    fields from the relevant place in /proc/<pid>/stat. This is part of the
    nsenter rewrite patchset (namely the pidns-orphaning part).
    
    A nice extension to this would be to use inotify and make
    libcontainer/system/proc.go entirely chan based.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- libcontainer/system/proc.go
+++ libcontainer/system/proc.go
@@ -1,3 +1,5 @@
+// +build linux
+
 package system
 
 import (
@@ -7,8 +9,11 @@ import (
 	"strings"
 )
 
-// look in /proc to find the process start time so that we can verify
-// that this pid has started after ourself
+// GetProcessStartTime reads /proc/<pid>/stat to determine the "start time" of
+// a process as provided by the kernel. The format of this start time is
+// defined by proc(5) and is not parsed or otherwise handled by us. Combined
+// with the process ID, this function allows for verification of whether or not
+// a particular PID is the same process at two different points in time.
 func GetProcessStartTime(pid int) (string, error) {
 	data, err := ioutil.ReadFile(filepath.Join("/proc", strconv.Itoa(pid), "stat"))
 	if err != nil {
@@ -24,4 +29,31 @@ func GetProcessStartTime(pid int) (string, error) {
 	// value was expressed in jiffies.  Since Linux 2.6, the value is expressed in  clock  ticks
 	// (divide by sysconf(_SC_CLK_TCK)).
 	return parts[22-1], nil // starts at 1
+}
+
+// GetProcessState reads /proc/<pid>/stat to determine the "state" of a process
+// as provided by the kernel. The format of the state is defined by proc(5) and
+// is not parsed by us, though it is usually a single character mnemonic.
+func GetProcessState(pid int) (string, error) {
+	data, err := ioutil.ReadFile(filepath.Join("/proc", strconv.Itoa(pid), "stat"))
+	if err != nil {
+		return "", err
+	}
+
+	parts := strings.Split(string(data), " ")
+	// the state field is located in pos 3
+	return parts[3-1], nil // starts at 1
+}
+
+// GetProcessParent reads reads /proc/<pid>/stat to determine the parent of a
+// process.
+func GetProcessParent(pid int) (int, error) {
+	data, err := ioutil.ReadFile(filepath.Join("/proc", strconv.Itoa(pid), "stat"))
+	if err != nil {
+		return -1, err
+	}
+
+	parts := strings.Split(string(data), " ")
+	// the state field is located in pos 4
+	return strconv.Atoi(parts[4-1]) // starts at 1
 }
