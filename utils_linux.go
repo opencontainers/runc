47e4e5d8fde756fe8e60733d227796ffc0876bc8
Zhang Wei <zhangwei555@huawei.com>
2016-10-14 10:23:12 +0800

    Small nit: remove unnecessary temp variable
    Signed-off-by: Zhang Wei <zhangwei555@huawei.com>

--- utils_linux.go
+++ utils_linux.go
@@ -295,7 +295,6 @@ func startContainer(context *cli.Context, spec *specs.Spec, create bool) (int, e
 	if err != nil {
 		return -1, err
 	}
-	detach := context.Bool("detach")
 	// Support on-demand socket activation by passing file descriptors into the container init process.
 	listenFDs := []*os.File{}
 	if os.Getenv("LISTEN_FDS") != "" {
@@ -307,7 +306,7 @@ func startContainer(context *cli.Context, spec *specs.Spec, create bool) (int, e
 		container:       container,
 		listenFDs:       listenFDs,
 		console:         context.String("console"),
-		detach:          detach,
+		detach:          context.Bool("detach"),
 		pidFile:         context.String("pid-file"),
 		create:          create,
 	}
