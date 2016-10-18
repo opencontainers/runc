opencontainers/runc

commit a60040c62d03894be74911760f13e860d5e323cd

Container must not checkpoint in created state
Signed-off-by: rajasec <rajasec79@gmail.com>

checkpoint.go
@@ -38,6 +38,13 @@ checkpointed.`,
 		if err != nil {
 			return err
 		}
+		status, err := container.Status()
+		if err != nil {
+			return(err)
+		}
+		if status == libcontainer.Created {
+			fatalf("Container cannot be checkpointed in created state")
+		}
 		defer destroy(container)
 		options := criuOptions(context)
 		// these are the mandatory criu options for a container

commit 38560a0316cae023f50aed42ab226271231a1012

checkpoint: fix gofmt
Fixes: a60040c62d03 ("Container must not checkpoint in created state")
Fixes: #1076
Signed-off-by: Aleksa Sarai <asarai@suse.de>

checkpoint.go
@@ -40,7 +40,7 @@ checkpointed.`,
 		}
 		status, err := container.Status()
 		if err != nil {
-			return(err)
+			return err
 		}
 		if status == libcontainer.Created {
 			fatalf("Container cannot be checkpointed in created state")