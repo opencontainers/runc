d44d9e1c6f7caf87a912447b2b7a74dfefd66ccd
John Howard <jhoward@microsoft.com>
2016-12-08 10:48:16 -0800

    Windows: Back to compiling
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- delete.go
+++ delete.go
@@ -1,4 +1,4 @@
-// +build !solaris
+// +build !solaris,!windows
 
 package main
 
---
+++ spec_windows.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// loadSpec loads the specification from the provided path.
+func loadSpec(cPath string) (spec *specs.Spec, err error) {
+	return nil, nil
+}
