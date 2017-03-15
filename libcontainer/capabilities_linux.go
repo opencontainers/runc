4f903a21c480443e8dbfc65b083fd05891501c67
Mrunal Patel <mrunalp@gmail.com>
2017-03-15 11:33:02 -0700

    Remove ambient build tag
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- libcontainer/capabilities_linux.go
+++ libcontainer/capabilities_linux.go
@@ -11,6 +11,8 @@ import (
 	"github.com/syndtr/gocapability/capability"
 )
 
+const allCapabilityTypes = capability.CAPS | capability.BOUNDS | capability.AMBS
+
 var capabilityMap map[string]capability.Cap
 
 func init() {
