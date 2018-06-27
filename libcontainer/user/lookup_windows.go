ee87fa1b34e66cad66d3700fa0c6c3ec9fcd8228
David Eads <deads@redhat.com>
2018-06-27 08:31:34 -0400

    fix cross build issue

--- libcontainer/user/lookup_windows.go
+++ libcontainer/user/lookup_windows.go
@@ -3,7 +3,9 @@
 package user
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os/user"
 )
 
@@ -37,4 +39,30 @@ func lookupGid(gid int) (Group, error) {
 		return Group{}, err
 	}
 	return groupFromOS(g)
+}
+
+var notSupported = errors.New("not supported in this build")
+
+func GetPasswdPath() (string, error) {
+	return "", notSupported
+}
+
+func GetPasswd() (io.ReadCloser, error) {
+	return nil, notSupported
+}
+
+func GetGroupPath() (string, error) {
+	return "", notSupported
+}
+
+func GetGroup() (io.ReadCloser, error) {
+	return nil, notSupported
+}
+
+func CurrentUser() (User, error) {
+	return User{}, notSupported
+}
+
+func CurrentGroup() (Group, error) {
+	return Group{}, notSupported
 }
