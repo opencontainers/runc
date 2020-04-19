opencontainers/runc

commit 87712d288e420f6b421519bd799258b2037ff7d0

checkpoint: remove error message with --leave-running
If checkpointing a container with '--leave-running' runc started to
print the following message:

 ERRO[0000] container is not destroyed

The message is correct, because CRIU did not terminate the process, but
as that was requested by the user the message is wrong.

So now the container is only destroyed if the user did not specify
'--leave-running'.

Signed-off-by: Adrian Reber <areber@redhat.com>

checkpoint.go
@@ -62,8 +62,15 @@ checkpointed.`,
 		if status == libcontainer.Created || status == libcontainer.Stopped {
 			fatalf("Container cannot be checkpointed in %s state", status.String())
 		}
-		defer destroy(container)
 		options := criuOptions(context)
+		if !options.LeaveRunning || !options.PreDump {
+			// destroy prints out an error if we tell CRIU to
+			// leave the container running:
+			// ERRO[0000] container is not destroyed
+			// The message is correct, but we actually do not want
+			// to destroy the container in this case.
+			defer destroy(container)
+		}
 		// these are the mandatory criu options for a container
 		setPageServer(context, options)
 		setManageCgroupsMode(context, options)

commit 3006db2bfe5ffc5be300907000c269ca6f0b0c21

checkpoint: don't print error if --pre-dump is set
Obviously, either --pre-dump or --leave-running implies
that we don't want to remove the container.

Fixes: 87712d288
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

checkpoint.go
@@ -63,12 +63,8 @@ checkpointed.`,
 			fatalf("Container cannot be checkpointed in %s state", status.String())
 		}
 		options := criuOptions(context)
-		if !options.LeaveRunning || !options.PreDump {
-			// destroy prints out an error if we tell CRIU to
-			// leave the container running:
-			// ERRO[0000] container is not destroyed
-			// The message is correct, but we actually do not want
-			// to destroy the container in this case.
+		if !(options.LeaveRunning || options.PreDump) {
+			// destroy container unless we tell CRIU to keep it
 			defer destroy(container)
 		}
 		// these are the mandatory criu options for a container