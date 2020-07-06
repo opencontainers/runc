d65b66a60a1e1f9e715381f2a490fcfa2ae41d5f
Stefan Berger <stefanb@linux.ibm.com>
2020-07-06 11:50:01 -0400

    maskedPaths: Add /sys/devices/virtual/tpm
    Add /sys/devices/virtual/tpm to the mask paths to avoid isolation
    issues via sysfs for TPM 1.2
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- libcontainer/specconv/example.go
+++ libcontainer/specconv/example.go
@@ -121,6 +121,7 @@ func Example() *specs.Spec {
 				"/proc/sched_debug",
 				"/sys/firmware",
 				"/proc/scsi",
+				"/sys/devices/virtual/tpm",
 			},
 			ReadonlyPaths: []string{
 				"/proc/bus",
