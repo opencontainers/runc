6249136a290a769263fd88d1795fbc0ae92ca342
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-08-04 15:07:22 +0900

    add libseccomp version to `runc --version`
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 
 	"github.com/opencontainers/runc/libcontainer/logs"
-
+	"github.com/opencontainers/runc/libcontainer/seccomp"
 	"github.com/opencontainers/runtime-spec/specs-go"
 
 	"github.com/sirupsen/logrus"
@@ -64,6 +64,10 @@ func main() {
 	}
 	v = append(v, fmt.Sprintf("spec: %s", specs.Version))
 	v = append(v, fmt.Sprintf("go: %s", runtime.Version()))
+	if seccomp.IsEnabled() {
+		major, minor, micro := seccomp.Version()
+		v = append(v, fmt.Sprintf("libseccomp: %d.%d.%d", major, minor, micro))
+	}
 	app.Version = strings.Join(v, "\n")
 
 	xdgRuntimeDir := ""
