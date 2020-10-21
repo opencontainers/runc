17de6f803d161f93f851e62296fc51f6d484bd50
Kir Kolyshkin <kolyshkin@gmail.com>
2020-10-21 09:57:32 -0700

    vendor: bump mountinfo to v0.4.0
    Release notes:
     - https://github.com/moby/sys/releases/tag/mountinfo%2Fv0.4.0
    
    In particular, this fixes a regression introduced in commit b8bf572812
    which checked the mountinfo.Root field in a FilterFunc, while Root was
    not set when calling a filter.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- vendor/github.com/moby/sys/mountinfo/mountinfo_filters.go
+++ vendor/github.com/moby/sys/mountinfo/mountinfo_filters.go
@@ -6,12 +6,12 @@ import "strings"
 // used to filter out mountinfo entries we're not interested in,
 // and/or stop further processing if we found what we wanted.
 //
-// It takes a pointer to the Info struct (not fully populated,
-// currently only Mountpoint, FSType, Source, and (on Linux)
-// VFSOptions are filled in), and returns two booleans:
+// It takes a pointer to the Info struct (fully populated with all available
+// fields on the GOOS platform), and returns two booleans:
 //
-//  - skip: true if the entry should be skipped
-//  - stop: true if parsing should be stopped after the entry
+// skip: true if the entry should be skipped;
+//
+// stop: true if parsing should be stopped after the entry.
 type FilterFunc func(*Info) (skip, stop bool)
 
 // PrefixFilter discards all entries whose mount points
@@ -36,8 +36,8 @@ func SingleEntryFilter(mp string) FilterFunc {
 // ParentsFilter returns all entries whose mount points
 // can be parents of a path specified, discarding others.
 //
-// For example, given `/var/lib/docker/something`, entries
-// like `/var/lib/docker`, `/var` and `/` are returned.
+// For example, given /var/lib/docker/something, entries
+// like /var/lib/docker, /var and / are returned.
 func ParentsFilter(path string) FilterFunc {
 	return func(m *Info) (bool, bool) {
 		skip := !strings.HasPrefix(path, m.Mountpoint)
