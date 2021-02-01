e0e17538ebebde472f5aef6b243850434fa199db
Sebastiaan van Stijn <github@gone.nl>
2021-02-01 11:51:16 +0100

    libcontainer/user: move userFromOS,groupFromOS to Windows files
    These are only used on Windows, causing the deadcode linter to complain:
    
        Error: `userFromOS` is unused (deadcode)
        Error: `groupFromOS` is unused (deadcode)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libcontainer/user/lookup_windows.go
+++ libcontainer/user/lookup_windows.go
@@ -38,3 +38,42 @@ func lookupGid(gid int) (Group, error) {
 	}
 	return groupFromOS(g)
 }
+
+// userFromOS converts an os/user.(*User) to local User
+//
+// (This does not include Pass, Shell or Gecos)
+func userFromOS(u *user.User) (User, error) {
+	newUser := User{
+		Name: u.Username,
+		Home: u.HomeDir,
+	}
+	id, err := strconv.Atoi(u.Uid)
+	if err != nil {
+		return newUser, err
+	}
+	newUser.Uid = id
+
+	id, err = strconv.Atoi(u.Gid)
+	if err != nil {
+		return newUser, err
+	}
+	newUser.Gid = id
+	return newUser, nil
+}
+
+// groupFromOS converts an os/user.(*Group) to local Group
+//
+// (This does not include Pass or List)
+func groupFromOS(g *user.Group) (Group, error) {
+	newGroup := Group{
+		Name: g.Name,
+	}
+
+	id, err := strconv.Atoi(g.Gid)
+	if err != nil {
+		return newGroup, err
+	}
+	newGroup.Gid = id
+
+	return newGroup, nil
+}
