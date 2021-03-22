249356a1a44112be0b894ae152207f599c5d7b15
Sebastiaan van Stijn <github@gone.nl>
2021-03-22 14:36:44 +0100

    libcontainer/system: remove unused GetParentNSeuid()
    This function was added in f103de57ec89137d86b77584f676b760ec7c7ba4, but no
    longer used since 06f789cf26774dd64cb2a9cc0b3c6a6ff832733bc (v1.0.0-rc6)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- libcontainer/system/unsupported.go
+++ libcontainer/system/unsupported.go
@@ -3,8 +3,6 @@
 package system
 
 import (
-	"os"
-
 	"github.com/opencontainers/runc/libcontainer/user"
 )
 
@@ -18,10 +16,4 @@ func RunningInUserNS() bool {
 // Always returns false
 func UIDMapInUserNS(uidmap []user.IDMap) bool {
 	return false
-}
-
-// GetParentNSeuid returns the euid within the parent user namespace
-// Always returns os.Geteuid on non-linux
-func GetParentNSeuid() int {
-	return os.Geteuid()
 }
