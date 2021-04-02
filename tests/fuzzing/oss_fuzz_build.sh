697d2211d4463d5fc11a816fd4087c5120c72b9e
AdamKorcz <adam@adalogics.com>
2021-04-02 11:34:28 +0100

    Fix oss-fuzz coverage build
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- tests/fuzzing/oss_fuzz_build.sh
+++ tests/fuzzing/oss_fuzz_build.sh
@@ -8,6 +8,15 @@
 # OSS-fuzz environment.
 # More info about compile_go_fuzzer() can be found here:
 #     https://google.github.io/oss-fuzz/getting-started/new-project-guide/go-lang/#buildsh
+
+if [[ $SANITIZER = *coverage* ]]; then
+        compile_go_fuzzer github.com/opencontainers/runc/libcontainer/system FuzzUIDMap id_map_fuzzer linux gofuzz
+        compile_go_fuzzer github.com/opencontainers/runc/libcontainer/user FuzzUser user_fuzzer gofuzz
+        compile_go_fuzzer github.com/opencontainers/runc/libcontainer/configs FuzzUnmarshalJSON configs_fuzzer gofuzz
+
+        exit 0
+fi
+
 compile_go_fuzzer ./libcontainer/system FuzzUIDMap id_map_fuzzer linux
 compile_go_fuzzer ./libcontainer/user FuzzUser user_fuzzer
 compile_go_fuzzer ./libcontainer/configs FuzzUnmarshalJSON configs_fuzzer
