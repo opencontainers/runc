6e74a715b14280dd45bc381a9a83c0adc1b992b6
Kir Kolyshkin <kolyshkin@gmail.com>
2021-06-29 17:23:58 -0700

    exec.go: nit
    No need to have an intermediate variable here.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- exec.go
+++ exec.go
@@ -122,7 +122,6 @@ func execProcess(context *cli.Context) (int, error) {
 	if path == "" && len(context.Args()) == 1 {
 		return -1, errors.New("process args cannot be empty")
 	}
-	detach := context.Bool("detach")
 	state, err := container.State()
 	if err != nil {
 		return -1, err
@@ -143,7 +142,7 @@ func execProcess(context *cli.Context) (int, error) {
 		shouldDestroy:   false,
 		container:       container,
 		consoleSocket:   context.String("console-socket"),
-		detach:          detach,
+		detach:          context.Bool("detach"),
 		pidFile:         context.String("pid-file"),
 		action:          CT_ACT_RUN,
 		init:            false,
