2c3792baf9e7219af5ca5586af39461d5b0ba93f
Kir Kolyshkin <kolyshkin@gmail.com>
2021-11-12 15:01:40 -0800

    libct/specconv: make mountFlags and extensionFlags global
    This makes the repeated calls to parseMountOptions faster,
    and decreases the amount of garbage to collect.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libcontainer/specconv/spec_linux.go
+++ libcontainer/specconv/spec_linux.go
@@ -26,9 +26,13 @@ import (
 )
 
 var (
-	initMapsOnce            sync.Once
-	namespaceMapping        map[specs.LinuxNamespaceType]configs.NamespaceType
-	mountPropagationMapping map[string]int
+	initMapsOnce               sync.Once
+	namespaceMapping           map[specs.LinuxNamespaceType]configs.NamespaceType
+	mountPropagationMapping    map[string]int
+	mountFlags, extensionFlags map[string]struct {
+		clear bool
+		flag  int
+	}
 )
 
 func initMaps() {
@@ -53,6 +57,51 @@ func initMaps() {
 			"runbindable": unix.MS_UNBINDABLE | unix.MS_REC,
 			"unbindable":  unix.MS_UNBINDABLE,
 			"":            0,
+		}
+
+		mountFlags = map[string]struct {
+			clear bool
+			flag  int
+		}{
+			"acl":           {false, unix.MS_POSIXACL},
+			"async":         {true, unix.MS_SYNCHRONOUS},
+			"atime":         {true, unix.MS_NOATIME},
+			"bind":          {false, unix.MS_BIND},
+			"defaults":      {false, 0},
+			"dev":           {true, unix.MS_NODEV},
+			"diratime":      {true, unix.MS_NODIRATIME},
+			"dirsync":       {false, unix.MS_DIRSYNC},
+			"exec":          {true, unix.MS_NOEXEC},
+			"iversion":      {false, unix.MS_I_VERSION},
+			"lazytime":      {false, unix.MS_LAZYTIME},
+			"loud":          {true, unix.MS_SILENT},
+			"mand":          {false, unix.MS_MANDLOCK},
+			"noacl":         {true, unix.MS_POSIXACL},
+			"noatime":       {false, unix.MS_NOATIME},
+			"nodev":         {false, unix.MS_NODEV},
+			"nodiratime":    {false, unix.MS_NODIRATIME},
+			"noexec":        {false, unix.MS_NOEXEC},
+			"noiversion":    {true, unix.MS_I_VERSION},
+			"nolazytime":    {true, unix.MS_LAZYTIME},
+			"nomand":        {true, unix.MS_MANDLOCK},
+			"norelatime":    {true, unix.MS_RELATIME},
+			"nostrictatime": {true, unix.MS_STRICTATIME},
+			"nosuid":        {false, unix.MS_NOSUID},
+			"rbind":         {false, unix.MS_BIND | unix.MS_REC},
+			"relatime":      {false, unix.MS_RELATIME},
+			"remount":       {false, unix.MS_REMOUNT},
+			"ro":            {false, unix.MS_RDONLY},
+			"rw":            {true, unix.MS_RDONLY},
+			"silent":        {false, unix.MS_SILENT},
+			"strictatime":   {false, unix.MS_STRICTATIME},
+			"suid":          {true, unix.MS_NOSUID},
+			"sync":          {false, unix.MS_SYNCHRONOUS},
+		}
+		extensionFlags = map[string]struct {
+			clear bool
+			flag  int
+		}{
+			"tmpcopyup": {false, configs.EXT_COPYUP},
 		}
 	})
 }
@@ -782,55 +831,11 @@ func parseMountOptions(options []string) (int, []int, string, int) {
 		extFlags int
 	)
 	initMaps()
-	flags := map[string]struct {
-		clear bool
-		flag  int
-	}{
-		"acl":           {false, unix.MS_POSIXACL},
-		"async":         {true, unix.MS_SYNCHRONOUS},
-		"atime":         {true, unix.MS_NOATIME},
-		"bind":          {false, unix.MS_BIND},
-		"defaults":      {false, 0},
-		"dev":           {true, unix.MS_NODEV},
-		"diratime":      {true, unix.MS_NODIRATIME},
-		"dirsync":       {false, unix.MS_DIRSYNC},
-		"exec":          {true, unix.MS_NOEXEC},
-		"iversion":      {false, unix.MS_I_VERSION},
-		"lazytime":      {false, unix.MS_LAZYTIME},
-		"loud":          {true, unix.MS_SILENT},
-		"mand":          {false, unix.MS_MANDLOCK},
-		"noacl":         {true, unix.MS_POSIXACL},
-		"noatime":       {false, unix.MS_NOATIME},
-		"nodev":         {false, unix.MS_NODEV},
-		"nodiratime":    {false, unix.MS_NODIRATIME},
-		"noexec":        {false, unix.MS_NOEXEC},
-		"noiversion":    {true, unix.MS_I_VERSION},
-		"nolazytime":    {true, unix.MS_LAZYTIME},
-		"nomand":        {true, unix.MS_MANDLOCK},
-		"norelatime":    {true, unix.MS_RELATIME},
-		"nostrictatime": {true, unix.MS_STRICTATIME},
-		"nosuid":        {false, unix.MS_NOSUID},
-		"rbind":         {false, unix.MS_BIND | unix.MS_REC},
-		"relatime":      {false, unix.MS_RELATIME},
-		"remount":       {false, unix.MS_REMOUNT},
-		"ro":            {false, unix.MS_RDONLY},
-		"rw":            {true, unix.MS_RDONLY},
-		"silent":        {false, unix.MS_SILENT},
-		"strictatime":   {false, unix.MS_STRICTATIME},
-		"suid":          {true, unix.MS_NOSUID},
-		"sync":          {false, unix.MS_SYNCHRONOUS},
-	}
-	extensionFlags := map[string]struct {
-		clear bool
-		flag  int
-	}{
-		"tmpcopyup": {false, configs.EXT_COPYUP},
-	}
 	for _, o := range options {
-		// If the option does not exist in the flags table or the flag
-		// is not supported on the platform,
-		// then it is a data value for a specific fs type
-		if f, exists := flags[o]; exists && f.flag != 0 {
+		// If the option does not exist in the mountFlags table,
+		// or the flag is not supported on the platform,
+		// then it is a data value for a specific fs type.
+		if f, exists := mountFlags[o]; exists && f.flag != 0 {
 			if f.clear {
 				flag &= ^f.flag
 			} else {
