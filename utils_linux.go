d23b8109279efdd60bc9a7afc03bcad7c0c378f2
Kir Kolyshkin <kolyshkin@gmail.com>
2021-11-29 19:31:01 -0800

    checkpoint: rm getDefaultImagePath arg
    It was never used.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- utils_linux.go
+++ utils_linux.go
@@ -66,7 +66,7 @@ func getContainer(context *cli.Context) (libcontainer.Container, error) {
 	return factory.Load(id)
 }
 
-func getDefaultImagePath(context *cli.Context) string {
+func getDefaultImagePath() string {
 	cwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
