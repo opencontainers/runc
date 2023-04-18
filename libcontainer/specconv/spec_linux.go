e6f297152d0e8073f614f5f81ec4a8a69b8aff0a
Paweł Szulik <paul.szulik@gmail.com>
2023-04-18 19:34:05 +0200

    Address the review comments.
    Signed-off-by: Paweł Szulik <paul.szulik@gmail.com>

--- libcontainer/specconv/spec_linux.go
+++ libcontainer/specconv/spec_linux.go
@@ -430,9 +430,9 @@ func CreateLibcontainerConfig(opts *CreateOpts) (*configs.Config, error) {
 				ClosID:        spec.Linux.IntelRdt.ClosID,
 				L3CacheSchema: spec.Linux.IntelRdt.L3CacheSchema,
 				MemBwSchema:   spec.Linux.IntelRdt.MemBwSchema,
+				EnableCMT:     spec.Linux.IntelRdt.EnableCMT,
+				EnableMBM:     spec.Linux.IntelRdt.EnableMBM,
 			}
-			config.IntelRdt.EnableCMT = spec.Linux.IntelRdt.EnableCMT
-			config.IntelRdt.EnableMBM = spec.Linux.IntelRdt.EnableMBM
 		}
 	}
 
