7abce9276f0dc6946685be77ef54cdeeebde2e77
Kir Kolyshkin <kolyshkin@gmail.com>
2023-04-26 14:01:47 -0700

    libct/cg/sd/v2: rely on systemd to set device rules
    It seems that the code added by commit b810da149008f1d7d0 had cgroup v1
    in mind, where runc overwrites the rules set by systemd. It is all
    different in v2, because both ebpf programs (systemd's and runc's) have
    to say "allow" for the device to get access.
    
    So, when using cgroup v2 with systemd cgroup driver, access to devices
    rules for that can't be translated to systemd properties is not possible
    at all, and it makes sense to error out (rather than warn) in such case,
    as the container won't work as intended.
    
    With this change in mind, provided that runc correctly translates all
    the device access rule, and systemd correctly applies those, we no
    longer have to create and apply a second eBPF program with own rules.
    Let's stop doing that, instead relying on systemd only.
    
    Having two sets of rules (two ebpf programs) for cgroupv2/ebpf is
    problematic for two reasons:
    
    1. Both sets should say "ok" for access to be granted (as explained by
       the previous commit).
    
    2. After systemd daemon-reload (which happens during routine systemd
       upgrade) the program runc adds is removed, so it's a time-bomb.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libcontainer/cgroups/systemd/common.go
+++ libcontainer/cgroups/systemd/common.go
@@ -38,7 +38,7 @@ var (
 	// [github.com/opencontainers/runc/libcontainer/cgroups/devices]
 	// package is imported, it is set to nil, so cgroup managers can't
 	// configure devices.
-	GenerateDeviceProps func(r *configs.Resources, sdVer int) ([]systemdDbus.Property, error)
+	GenerateDeviceProps func(r *configs.Resources, sdVer, cgroupVer int) ([]systemdDbus.Property, error)
 )
 
 // NOTE: This function comes from package github.com/coreos/go-systemd/util
@@ -351,7 +351,7 @@ func addCpuset(cm *dbusConnManager, props *[]systemdDbus.Property, cpus, mems st
 
 // generateDeviceProperties takes the configured device rules and generates a
 // corresponding set of systemd properties to configure the devices correctly.
-func generateDeviceProperties(r *configs.Resources, cm *dbusConnManager) ([]systemdDbus.Property, error) {
+func generateDeviceProperties(r *configs.Resources, cm *dbusConnManager, cgroupVersion int) ([]systemdDbus.Property, error) {
 	if GenerateDeviceProps == nil {
 		if len(r.Devices) > 0 {
 			return nil, cgroups.ErrDevicesUnsupported
@@ -359,5 +359,5 @@ func generateDeviceProperties(r *configs.Resources, cm *dbusConnManager) ([]syst
 		return nil, nil
 	}
 
-	return GenerateDeviceProps(r, systemdVersion(cm))
+	return GenerateDeviceProps(r, systemdVersion(cm), cgroupVersion)
 }
