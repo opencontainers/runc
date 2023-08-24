42b629ec23c984e1aac8a94b7500d62b9303a04a
TTFISH <jiongchiyu@gmail.com>
2023-08-25 02:50:41 +0800

    fix ossfuzz building failure
    Signed-off-by: TTFISH <jiongchiyu@gmail.com>

--- tests/fuzzing/oss_fuzz_build.sh
+++ tests/fuzzing/oss_fuzz_build.sh
@@ -8,6 +8,25 @@
 # OSS-fuzz environment.
 # More info about compile_go_fuzzer() can be found here:
 #     https://google.github.io/oss-fuzz/getting-started/new-project-guide/go-lang/#buildsh
+set -o nounset
+set -o pipefail
+set -o errexit
+set -x
+
+# specifiy go version
+apt-get update && apt-get install -y wget
+cd $SRC
+
+wget --quiet https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+mkdir tmp-go
+rm -rf /root/.go/*
+tar -C tmp-go/ -xzf go1.21.0.linux-amd64.tar.gz
+mv tmp-go/go/* /root/.go/
+
+# supporting cncf-fuzzing
+cd $SRC/runc
+go mod tidy
+
 compile_go_fuzzer github.com/opencontainers/runc/libcontainer/userns FuzzUIDMap id_map_fuzzer linux,gofuzz
 compile_go_fuzzer github.com/opencontainers/runc/libcontainer/user FuzzUser user_fuzzer
 compile_go_fuzzer github.com/opencontainers/runc/libcontainer/configs FuzzUnmarshalJSON configs_fuzzer
