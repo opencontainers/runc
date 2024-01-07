9cbab117bc61a72aba6b3585a3aecbc3a8507cf8
lifubang <lifubang@acmcoder.com>
2024-01-07 13:49:28 +0800

    helpers.bash: no need to add word pattern operator in awk
    Signed-off-by: lifubang <lifubang@acmcoder.com>

--- tests/integration/helpers.bash
+++ tests/integration/helpers.bash
@@ -132,7 +132,7 @@ function init_cgroup_paths() {
 		CGROUP_SUBSYSTEMS=$(awk '!/^#/ {print $1}' /proc/cgroups)
 		local g base_path
 		for g in ${CGROUP_SUBSYSTEMS}; do
-			base_path=$(awk '$(NF-2) == "cgroup" && $NF ~ /\<'"${g}"'\>/ { print $5; exit }' /proc/self/mountinfo)
+			base_path=$(awk '$(NF-2) == "cgroup" && ($NF ~ /'"${g}"',/ || $NF ~ /'"${g}"'$/) { print $5; exit }' /proc/self/mountinfo)
 			test -z "$base_path" && continue
 			eval CGROUP_"${g^^}"_BASE_PATH="${base_path}"
 		done
