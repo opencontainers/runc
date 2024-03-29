d7a29a3b33675c57daa3134c01bfbc8b8375374c
ls-ggg <335814617@qq.com>
2024-03-29 18:12:08 +0800

    libct: clean cached rlimit nofile in go runtime
    As reported in issue #4195, the new version(since 1.19) of go runtime
    will cache rlimit-nofile. Before executing execve, the rlimit-nofile
    of the process will be restored with the cache. In runc, this will
    cause the rlimit-nofile set by the parent process for the container
    to become invalid. It can be solved by clearing the cache.
    
    Signed-off-by: ls-ggg <335814617@qq.com>
    (cherry picked from commit f9f8abf3102e47f2fc3aa45bcd134e957da7de66)
    Signed-off-by: lifubang <lifubang@acmcoder.com>
    (cherry picked from commit da68c8e37b2f281c2105d6f7375d75d84db13a0f)
    Signed-off-by: lifubang <lifubang@acmcoder.com>

---
+++ libcontainer/system/rlimit_go119.go
@@ -0,0 +1,24 @@
+//go:build go1.19
+
+package system
+
+import (
+	"sync/atomic"
+	"syscall"
+
+	_ "unsafe" // for go:linkname
+)
+
+//go:linkname syscallOrigRlimitNofile syscall.origRlimitNofile
+var syscallOrigRlimitNofile atomic.Pointer[syscall.Rlimit]
+
+// As reported in issue #4195, the new version of go runtime(since 1.19)
+// will cache rlimit-nofile. Before executing execve, the rlimit-nofile
+// of the process will be restored with the cache. In runc, this will
+// cause the rlimit-nofile setting by the parent process for the container
+// to become invalid. It can be solved by clearing this cache. But
+// unfortunately, go stdlib doesn't provide such function, so we need to
+// link to the private var `origRlimitNofile` in package syscall to hack.
+func ClearRlimitNofileCache() {
+	syscallOrigRlimitNofile.Store(nil)
+}
