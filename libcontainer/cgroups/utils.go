6569ae665fe0b2df490e604c3cc26618f4d12b22
Kir Kolyshkin <kolyshkin@gmail.com>
2024-07-22 12:41:24 -0700

    runc update: fix updating swap for cgroup v2
    This allows to do
    
    	runc update $ID --memory=-1 --memory-swap=$VAL
    
    for cgroup v2, i.e. set memory to unlimited and swap to a specific
    value.
    
    This was not possible because ConvertMemorySwapToCgroupV2Value rejected
    memory=-1 ("unlimited"). In a hindsight, it was a mistake, because if
    memory limit is unlimited, we should treat memory+swap limit as just swap
    limit.
    
    While at it, improve some comments in the code, and revise the unit
    test.
    
    Fixes: c86be8a2 ("cgroupv2: fix setting MemorySwap")
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libcontainer/cgroups/utils.go
+++ libcontainer/cgroups/utils.go
@@ -415,23 +415,23 @@ func ConvertCPUSharesToCgroupV2Value(cpuShares uint64) uint64 {
 
 // ConvertMemorySwapToCgroupV2Value converts MemorySwap value from OCI spec
 // for use by cgroup v2 drivers. A conversion is needed since Resources.MemorySwap
-// is defined as memory+swap combined, while in cgroup v2 swap is a separate value.
+// is defined as memory+swap combined, while in cgroup v2 swap is a separate value,
+// so we need to subtract memory from it where it makes sense.
 func ConvertMemorySwapToCgroupV2Value(memorySwap, memory int64) (int64, error) {
-	// for compatibility with cgroup1 controller, set swap to unlimited in
+	// For compatibility with cgroup1 controller, set swap to unlimited in
 	// case the memory is set to unlimited, and swap is not explicitly set,
 	// treating the request as "set both memory and swap to unlimited".
 	if memory == -1 && memorySwap == 0 {
 		return -1, nil
 	}
-	if memorySwap == -1 || memorySwap == 0 {
-		// -1 is "max", 0 is "unset", so treat as is
+	if memorySwap == -1 || memorySwap == 0 || // Treat -1 ("max") and 0 ("unset") swap as is.
+		memory == -1 { // Unlimited memory, so treat swap as is.
 		return memorySwap, nil
 	}
-	// sanity checks
-	if memory == 0 || memory == -1 {
+	if memory == 0 { // Unset or unknown -- can't calculate swap.
 		return 0, errors.New("unable to set swap limit without memory limit")
 	}
-	if memory < 0 {
+	if memory < 0 { // Does not make sense to subtract negative value.
 		return 0, fmt.Errorf("invalid memory value: %d", memory)
 	}
 	if memorySwap < memory {
