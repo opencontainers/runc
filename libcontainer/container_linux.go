275032399904cbf2381043cedcd06ae232b202e8
Kir Kolyshkin <kolyshkin@gmail.com>
2025-01-07 19:32:27 -0800

    libct: simplify Caps inheritance
    For all other properties that are available in both Config and Process,
    the merging is performed by newInitConfig.
    
    Let's do the same for Capabilities for the sake of code uniformity.
    
    Also, thanks to the previous commit, we no longer have to make sure we
    do not call capabilities.New(nil).
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- libcontainer/container_linux.go
+++ libcontainer/container_linux.go
@@ -692,7 +692,7 @@ func (c *Container) newInitConfig(process *Process) *initConfig {
 		User:             process.User,
 		AdditionalGroups: process.AdditionalGroups,
 		Cwd:              process.Cwd,
-		Capabilities:     process.Capabilities,
+		Capabilities:     c.config.Capabilities,
 		PassedFilesCount: len(process.ExtraFiles),
 		ContainerID:      c.ID(),
 		NoNewPrivileges:  c.config.NoNewPrivileges,
@@ -706,6 +706,9 @@ func (c *Container) newInitConfig(process *Process) *initConfig {
 
 	// Overwrite config properties with ones from process.
 
+	if process.Capabilities != nil {
+		cfg.Capabilities = process.Capabilities
+	}
 	if process.NoNewPrivileges != nil {
 		cfg.NoNewPrivileges = *process.NoNewPrivileges
 	}
