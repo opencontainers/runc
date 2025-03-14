471094e28abf5f49fe170caaa37f4946ebf3a2ae
Rodrigo Campos <rodrigoca@microsoft.com>
2025-03-14 18:21:48 +0100

    libct: Use RetryOnEINTR for unix.Dup3()
    Signed-off-by: Rodrigo Campos <rodrigoca@microsoft.com>

--- libcontainer/rootfs_linux.go
+++ libcontainer/rootfs_linux.go
@@ -883,7 +883,10 @@ func reOpenDevNull() error {
 		}
 		if stat.Rdev == devNullStat.Rdev {
 			// Close and re-open the fd.
-			if err := unix.Dup3(int(file.Fd()), fd, 0); err != nil {
+			err := utils.RetryOnEINTR(func() error {
+				return unix.Dup3(int(file.Fd()), fd, 0)
+			})
+			if err != nil {
 				return &os.PathError{
 					Op:   "dup3",
 					Path: "fd " + strconv.Itoa(int(file.Fd())),
