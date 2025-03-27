0ef29ceaed332a54ded80c7504d8b1ebb98aa72c
Antti Kervinen <antti.kervinen@intel.com>
2025-03-27 15:42:24 +0200

    Add memory policy support
    Implement support for Linux memory policy in OCI spec PR:
    https://github.com/opencontainers/runtime-spec/pull/1282
    
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

---
+++ libcontainer/configs/memorypolicy.go
@@ -0,0 +1,29 @@
+package configs
+
+import "golang.org/x/sys/unix"
+
+//nolint:revive // ignore ALL_CAPS errors from revive and 2nd staticheck
+const (
+	MPOL_DEFAULT = iota
+	MPOL_PREFERRED
+	MPOL_BIND
+	MPOL_INTERLEAVE
+	MPOL_LOCAL
+	MPOL_PREFERRED_MANY
+	MPOL_WEIGHTED_INTERLEAVE
+
+	MPOL_F_STATIC_NODES   = 1 << 15
+	MPOL_F_RELATIVE_NODES = 1 << 14
+	MPOL_F_NUMA_BALANCING = 1 << 13
+)
+
+// LinuxMemoryPolicy contains memory policy configuration.
+type LinuxMemoryPolicy struct {
+	// Mode specifies memory policy mode without mode flags. See
+	// set_mempolicy() documentation for details.
+	Mode uint
+	// Flags contains mode flags.
+	Flags []uint
+	// Nodes contains NUMA nodes to which the mode applies.
+	Nodes *unix.CPUSet
+}
