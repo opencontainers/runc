1d78cb2112280bd283fec3dd169b1df896f35b90
Kir Kolyshkin <kolyshkin@gmail.com>
2025-04-09 10:10:50 -0700

    Completely remove --criu option
    This option is ignored since commit 6e1d476a, it's now time to actually
    remove it.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- main.go
+++ main.go
@@ -101,11 +101,6 @@ func main() {
 			Value: root,
 			Usage: "root directory for storage of container state (this should be located in tmpfs)",
 		},
-		cli.StringFlag{
-			Name:   "criu",
-			Usage:  "(obsoleted; do not use)",
-			Hidden: true,
-		},
 		cli.BoolFlag{
 			Name:  "systemd-cgroup",
 			Usage: "enable systemd cgroup support, expects cgroupsPath to be of form \"slice:prefix:name\" for e.g. \"system.slice:runc:434234\"",
@@ -151,10 +146,6 @@ func main() {
 		}
 		if err := reviseRootDir(context); err != nil {
 			return err
-		}
-		// TODO: remove this in runc 1.3.0.
-		if context.IsSet("criu") {
-			fmt.Fprintln(os.Stderr, "WARNING: --criu ignored (criu binary from $PATH is used); do not use")
 		}
 
 		return configLogrus(context)
