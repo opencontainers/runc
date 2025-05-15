531ef794e4ecd628006a865ad334a048ee2b4b2e
Aleksa Sarai <cyphar@cyphar.com>
2025-05-15 16:12:21 +1000

    console: use TIOCGPTPEER when allocating peer PTY
    When opening the peer end of a pty, the old kernel API required us to
    open /dev/pts/$num inside the container (at least since we fixed console
    handling many years ago in commit 244c9fc426ae ("*: console rewrite")).
    
    The problem is that in a hostile container it is possible for
    /dev/pts/$num to be an attacker-controlled symlink that runc can be
    tricked into resolving when doing bind-mounts. This allows the attacker
    to (among other things) persist /proc/... entries that are later masked
    by runc, allowing an attacker to escape through the kernel.core_pattern
    sysctl (/proc/sys/kernel/core_pattern). This is the original issue
    reported by Lei Wang and Li Fu Bang in CVE-2025-52565.
    
    However, it should be noted that this is not entirely a newly-discovered
    problem. Way back in Linux 4.13 (2017), I added the TIOCGPTPEER ioctl,
    which allows us to get a pty peer without touching the /dev/pts inside
    the container. The original threat model was around an attacker
    replacing /dev/pts/$n or /dev/pts/ptmx with some malicious inode (a DoS
    inode, or possibly a PTY they wanted a confused deputy to operate on).
    Unfortunately, there was no practical way for runc to cache a safe
    O_PATH handle to /dev/pts/ptmx (unlike other runtimes like LXC, which
    switched to TIOCGPTPEER way back in 2017). Since it wasn't clear how we
    could protect against the main attack TIOCGPTPEER was meant to protect
    against, we never switched to it (even though I implemented it
    specifically to harden container runtimes).
    
    Unfortunately, It turns out that mount *sources* are a threat we didn't
    fully consider. Since TIOCGPTPEER already solves this problem entirely
    for us in a race free way, we should just use that. In a later patch, we
    will add some hardening for /dev/pts/$num opening to maintain support
    for very old kernels (Linux 4.13 is very old at this point, but RHEL 7
    is still kicking and is stuck on Linux 3.10).
    
    Fixes: GHSA-qw9x-cqr3-wc7r CVE-2025-52565
    Reported-by: Lei Wang <ssst0n3@gmail.com> (CVE-2025-52565)
    Reported-by: lfbzhm <lifubang@acmcoder.com> (CVE-2025-52565)
    Reported-by: Aleksa Sarai <cyphar@cyphar.com> (TIOCGPTPEER)
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- internal/linux/linux.go
+++ internal/linux/linux.go
@@ -85,3 +85,23 @@ func SetMempolicy(mode uint, mask *unix.CPUSet) error {
 	})
 	return os.NewSyscallError("set_mempolicy", err)
 }
+
+// GetPtyPeer is a wrapper for ioctl(TIOCGPTPEER).
+func GetPtyPeer(ptyFd uintptr, unsafePeerPath string, flags int) (*os.File, error) {
+	// Make sure O_NOCTTY is always set -- otherwise runc might accidentally
+	// gain it as a controlling terminal. O_CLOEXEC also needs to be set to
+	// make sure we don't leak the handle either.
+	flags |= unix.O_NOCTTY | unix.O_CLOEXEC
+
+	// There is no nice wrapper for this kind of ioctl in unix.
+	peerFd, _, errno := unix.Syscall(
+		unix.SYS_IOCTL,
+		ptyFd,
+		uintptr(unix.TIOCGPTPEER),
+		uintptr(flags),
+	)
+	if errno != 0 {
+		return nil, os.NewSyscallError("ioctl TIOCGPTPEER", errno)
+	}
+	return os.NewFile(peerFd, unsafePeerPath), nil
+}
