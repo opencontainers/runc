db19bbed5348847da433faa9d69e9f90192bfa64
Aleksa Sarai <cyphar@cyphar.com>
2025-07-22 16:23:28 +1000

    internal/sys: add VerifyInode helper
    This will be used for a few security patches in later patches in this
    patchset. The need to verify what kind of inode we are operating on in a
    race-free way turns out to be quite a common pattern...
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

---
+++ internal/sys/doc.go
@@ -0,0 +1,5 @@
+// Package sys is an internal package that contains helper methods for dealing
+// with Linux that are more complicated than basic wrappers. Basic wrappers
+// usually belong in internal/linux. If you feel something belongs in
+// libcontainer/utils or libcontainer/system, it probably belongs here instead.
+package sys
