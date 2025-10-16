opencontainers/runc

commit d31e6b87cad915c0792d2f54d21c09822885333f

ci: bump bats to v0.11.0
This is the version available from Fedora 41.

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

.cirrus.yml
@@ -12,7 +12,7 @@ task:
     HOME: /root
     CIRRUS_WORKING_DIR: /home/runc
     GO_VER_PREFIX: "1.24."
-    BATS_VERSION: "v1.9.0"
+    BATS_VERSION: "v1.11.0"
     RPMS: gcc git-core iptables jq glibc-static libseccomp-devel make criu fuse-sshfs container-selinux
     # yamllint disable rule:key-duplicates
     matrix:

Dockerfile
@@ -1,5 +1,5 @@
 ARG GO_VERSION=1.23
-ARG BATS_VERSION=v1.9.0
+ARG BATS_VERSION=v1.11.0
 ARG LIBSECCOMP_VERSION=2.5.6
 
 FROM golang:${GO_VERSION}-bookworm

commit 6af1d637bace0493fdb4dad4177da7624ce22ab4

ci: bump bats to 1.11.1
Bump bats to the version from Fedora 42 (used in "fedora" job), so we
have the same version everywhere.

This also fixes an issue introduced by commit d31e6b87 (which forgot to
bump bats in GHA CI), and adds a note to the yaml in order to avoid the
same issue in the future.

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

.cirrus.yml
@@ -12,7 +12,7 @@ task:
     HOME: /root
     CIRRUS_WORKING_DIR: /home/runc
     GO_VER_PREFIX: "1.24."
-    BATS_VERSION: "v1.11.0"
+    BATS_VERSION: "v1.11.1"
     RPMS: gcc git-core iptables jq glibc-static libseccomp-devel make criu fuse-sshfs container-selinux
     # yamllint disable rule:key-duplicates
     matrix:

.github/workflows/test.yml
@@ -107,7 +107,7 @@ jobs:
     - name: Setup Bats and bats libs
       uses: bats-core/bats-action@3.0.1
       with:
-        bats-version: 1.9.0
+        bats-version: 1.11.1 # Known as BATS_VERSION in other places.
         support-install: false
         assert-install: false
         detik-install: false

Dockerfile
@@ -1,5 +1,5 @@
 ARG GO_VERSION=1.24
-ARG BATS_VERSION=v1.11.0
+ARG BATS_VERSION=v1.11.1
 ARG LIBSECCOMP_VERSION=2.5.6
 
 FROM golang:${GO_VERSION}-bookworm