ecb718c92bc6889bf63ed031bb7a9dcff09760a6
Kir Kolyshkin <kolyshkin@gmail.com>
2025-12-05 14:14:04 -0800

    Remove crypto/tls dependency
    It appears that when we import github.com/coreos/go-systemd/activation,
    it brings in the whole crypto/tls package (which is not used by runc
    directly or indirectly), making the runc binary size larger and
    potentially creating issues with FIPS compliance.
    
    Let's copy the code of function we use from go-systemd/activation
    to avoid that.
    
    The space savings are:
    
    $ size runc.before runc.after
       text	   data	    bss	    dec	    hex	filename
    7101084	5049593	 271560	12422237	 bd8c5d	runc.before
    6508796	4623281	 229128	11361205	 ad5bb5	runc.after
    
    Reported-by: Dimitri John Ledkov <dimitri.ledkov@surgut.co.uk>
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- internal/systemd/files_unix.go
+++ internal/systemd/files_unix.go
@@ -14,8 +14,8 @@
 
 //go:build !windows
 
-// Package activation implements primitives for systemd socket activation.
-package activation
+// Package systemd contains systemd related functions for runc.
+package systemd
 
 import (
 	"os"
@@ -24,18 +24,22 @@ import (
 	"syscall"
 )
 
+// The code below is copied from
+// https://github.com/coreos/go-systemd/blob/ce60782c0aabb616faa8e60f91e639d91f631e99/activation/files_unix.go
+// to avoid dependency on crypto/tls.
+
 const (
 	// listenFdsStart corresponds to `SD_LISTEN_FDS_START`.
 	listenFdsStart = 3
 )
 
-// Files returns a slice containing a `os.File` object for each
+// ActivationFiles returns a slice containing a `os.File` object for each
 // file descriptor passed to this process via systemd fd-passing protocol.
 //
 // The order of the file descriptors is preserved in the returned slice.
 // `unsetEnv` is typically set to `true` in order to avoid clashes in
 // fd usage and to avoid leaking environment flags to child processes.
-func Files(unsetEnv bool) []*os.File {
+func ActivationFiles(unsetEnv bool) []*os.File {
 	if unsetEnv {
 		defer func() {
 			// Unsetenv implementation for unix never returns an error.
@@ -51,7 +55,7 @@ func Files(unsetEnv bool) []*os.File {
 	}
 
 	nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
-	if err != nil || nfds == 0 {
+	if err != nil || nfds <= 0 {
 		return nil
 	}
 
