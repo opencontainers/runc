opencontainers/runc

commit 531e29e192113a2cae2d13b48901ec456175cb1e

script/lib.sh: set GOARM=5 for armel, GOARM=6 for armhf
"armhf" means ARMv7 for Debian, ARMv6 for Raspbian.
ARMv6 is chosen here for compatibility.

https://wiki.debian.org/RaspberryPi

> Raspberry Pi OS builds a single image for all of the Raspberry families,
> so you will get an armhf 32-bit, hard floating-point system, but built
> for the ARMv6 ISA (with VFP2), unlike Debian's ARMv7 ISA (with VFP3)
> port.

Prior to this commit, the script was setting GOARM=6 for armel,
GOARM=7 for armhf.

Fix issue 4033

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

script/lib.sh
@@ -45,12 +45,21 @@ function set_cross_vars() {
 	armel)
 		HOST=arm-${PLATFORM}eabi
 		GOARCH=arm
-		GOARM=6
+		GOARM=5
 		;;
 	armhf)
 		HOST=arm-${PLATFORM}eabihf
 		GOARCH=arm
-		GOARM=7
+		# "armhf" means ARMv7 for Debian, ARMv6 for Raspbian.
+		# ARMv6 is chosen here for compatibility.
+		#
+		# https://wiki.debian.org/RaspberryPi
+		#
+		# > Raspberry Pi OS builds a single image for all of the Raspberry families,
+		# > so you will get an armhf 32-bit, hard floating-point system, but built
+		# > for the ARMv6 ISA (with VFP2), unlike Debian's ARMv7 ISA (with VFP3)
+		# > port.
+		GOARM=6
 		;;
 	ppc64le)
 		HOST=powerpc64le-${PLATFORM}

commit c8a53cbc5988ad5653f23b1c197f1e490964c25c

build: treat armhf as ARMv7
The intention of commit 531e29e19211 ("script/lib.sh: set GOARM=5 for
armel, GOARM=6 for armhf") was to properly support older ARM platforms
with our release builds.

However, we have never been able to support ARMv6 for our builds because
we use the Debian compiler to build the libseccomp we statically compile
into our binaries and (as per the now-deleted comment itself) Debian
treats armhf as being ARMv7 so the final binaries we produced were
always only ever compatible with ARMv7+.

This was a bit of an oddity before but when building libpathrs for
releases we will need to use Rust which makes the target more explicit
(and while it does support armhf, we are using the Debian-packaged Rust
cross-compiler and thus are in the same dilemma with what Debian
considers "armhf" to be).

All-in-all, it's better to just bite the bullet and just follow Debian
here properly.

Fixes: 531e29e19211 ("script/lib.sh: set GOARM=5 for armel, GOARM=6 for armhf")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

script/lib.sh
@@ -48,16 +48,7 @@ function set_cross_vars() {
 	armhf)
 		HOST=arm-${PLATFORM}eabihf
 		GOARCH=arm
-		# "armhf" means ARMv7 for Debian, ARMv6 for Raspbian.
-		# ARMv6 is chosen here for compatibility.
-		#
-		# https://wiki.debian.org/RaspberryPi
-		#
-		# > Raspberry Pi OS builds a single image for all of the Raspberry families,
-		# > so you will get an armhf 32-bit, hard floating-point system, but built
-		# > for the ARMv6 ISA (with VFP2), unlike Debian's ARMv7 ISA (with VFP3)
-		# > port.
-		GOARM=6
+		GOARM=7
 		;;
 	ppc64le)
 		HOST=powerpc64le-${PLATFORM}