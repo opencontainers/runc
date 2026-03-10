opencontainers/runc

commit 8b2b5e9492262f575b44f449795b25b1874320d5

libct: remove relabeling dead code
There is no way to set Mount.Relabel field via OCI spec (config.json),
and so the relabeling code is never used.

My guess it's a leftover from times when runc used to be part of Docker.

Remove it, and mark Relabel field as deprecated.

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/configs/mount_linux.go
@@ -43,8 +43,10 @@ type Mount struct {
 	// Mount data applied to the mount.
 	Data string `json:"data,omitempty"`
 
-	// Relabel source if set, "z" indicates shared, "Z" indicates unshared.
-	Relabel string `json:"relabel,omitempty"`
+	// Relabel field is ignored.
+	//
+	// Deprecated: do not use. This field will be removed in runc 1.7.
+	Relabel string
 
 	// RecAttr represents mount properties to be applied recursively (AT_RECURSIVE), see mount_setattr(2).
 	RecAttr *unix.MountAttr `json:"rec_attr,omitempty"`

libcontainer/rootfs_linux.go
@@ -747,16 +747,6 @@ func mountToRootfs(c *mountConfig, m mountEntry) error {
 				return fmt.Errorf("failed to set user-requested vfs flags on bind-mount: %w", err)
 			}
 		}
-
-		if m.Relabel != "" {
-			if err := label.Validate(m.Relabel); err != nil {
-				return err
-			}
-			shared := label.IsShared(m.Relabel)
-			if err := label.Relabel(m.Source, mountLabel, shared); err != nil {
-				return err
-			}
-		}
 		return setRecAttr(m)
 	case "cgroup":
 		if cgroups.IsCgroup2UnifiedMode() {

commit d2abe476898702b1d2a2e1f114c8b020e4d38d03

libct/configs: exclude Relabel from json [un]marshaling
When deprecating Relabel field, its json attributes were mistakenly
removed, so now it is:
 - saved to JSON under "Relabel" (rather than "relabel");
 - won't be ignored if empty.

Let's fix it before it's too late.

Fixes: 8b2b5e94 ("libct: remove relabeling dead code")

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

libcontainer/configs/mount_linux.go
@@ -46,7 +46,7 @@ type Mount struct {
 	// Relabel field is ignored.
 	//
 	// Deprecated: do not use. This field will be removed in runc 1.7.
-	Relabel string
+	Relabel string `json:"-"`
 
 	// RecAttr represents mount properties to be applied recursively (AT_RECURSIVE), see mount_setattr(2).
 	RecAttr *unix.MountAttr `json:"rec_attr,omitempty"`