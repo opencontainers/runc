b345c78dcae5ac0edad610fb6e71cb5db61f6842
Aleksa Sarai <aleksa@amutable.com>
2026-04-02 11:25:43 +1100

    libct/devices: deprecate in favour of moby/sys/devices
    The libcontainer/devices package has been moved to moby/sys/devices, so
    we can just point users to that and keep some compatibility shims around
    until runc 1.6. We don't use it at all so there are no other changes
    needed.
    
    Signed-off-by: Aleksa Sarai <aleksa@amutable.com>

---
+++ libcontainer/devices/device_deprecated_unix.go
@@ -0,0 +1,47 @@
+//go:build !windows
+
+// Package devices provides some helper functions for constructing device
+// configurations for runc. These are exclusively used by higher-level runtimes
+// that need to configure runc's device list based on existing devices.
+//
+// Deprecated: Use github.com/moby/sys/devices instead. This package will be
+// removed in runc 1.6.
+package devices
+
+import (
+	"github.com/moby/sys/devices"
+	"github.com/opencontainers/cgroups/devices/config"
+)
+
+// ErrNotADevice denotes that a file is not a valid linux device.
+//
+// Deprecated: Use [devices.ErrNotADevice] instead. This package will be
+// removed in runc 1.6.
+var ErrNotADevice = devices.ErrNotADevice
+
+// DeviceFromPath takes the path to a device and its cgroup_permissions (which
+// cannot be easily queried) to look up the information about a linux device
+// and returns that information as a Device struct.
+//
+// Deprecated: Use [devices.DeviceFromPath] instead. This package will be
+// removed in runc 1.6.
+func DeviceFromPath(path, permissions string) (*config.Device, error) {
+	return devices.DeviceFromPath(path, permissions)
+}
+
+// HostDevices returns all devices that can be found under /dev directory.
+//
+// Deprecated: Use [devices.HostDevices] instead. This package will be
+// removed in runc 1.6.
+func HostDevices() ([]*config.Device, error) {
+	return devices.HostDevices()
+}
+
+// GetDevices recursively traverses a directory specified by path
+// and returns all devices found there.
+//
+// Deprecated: Use [devices.GetDevices] instead. This package will be
+// removed in runc 1.6.
+func GetDevices(path string) ([]*config.Device, error) {
+	return devices.GetDevices(path)
+}
