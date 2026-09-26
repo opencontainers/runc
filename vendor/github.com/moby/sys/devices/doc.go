ee6829a791e7dabd54ad1395718a0006d8b59368
Kir Kolyshkin <kolyshkin@gmail.com>
2026-09-26 13:51:03 -0700

    libct/devices: remove deprecated package
    This package was deprecated in runc 1.5 (in favor of
    github.com/moby/sys/devices), with a note that it will be removed
    in runc 1.6.
    
    Remove it. As a result, github.com/moby/sys/devices is no longer
    a runc dependency.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- vendor/github.com/moby/sys/devices/doc.go
+++
@@ -1,4 +0,0 @@
-// Package devices provides some helper functions for constructing device
-// configurations for runc. These are exclusively used by higher-level runtimes
-// that need to configure runc's device list based on existing devices.
-package devices
